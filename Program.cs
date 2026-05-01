ad0ef72739d441fa28828180eeefea53a771f358
Jonathan <jdlopes97@gmail.com>
2026-04-30 22:03:21 -0300

    estrutura inicial criada

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.CommandLine;
+
+RootCommand root = new RootCommand("My CLI app description");
+
+Argument<string> nameArgument = new("name")
+{
+    Description = "a name to greet",
+    DefaultValueFactory = result => "World"
+};
+
+Command greetCommand = new Command("greet", "Greet the user by name")
+{
+    nameArgument
+};
+
+greetCommand.SetAction( result => Console.WriteLine($"Hello, {result.GetValue(nameArgument)}") );
+
+root.Subcommands.Add(greetCommand);
+
+ParseResult parseResult = root.Parse(args);
+
+return parseResult.Invoke();
